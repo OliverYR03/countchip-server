98485f065f12d99322e2222810265b6482a8c86a
Oliver Ynfantas <115576310+OliverYR03@users.noreply.github.com>
2023-12-15 12:57:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,30 +21,19 @@ def modificar_imagen():
         args = request.args
         count_type = args.get('count_type')
 
-        # Lee la imagen con OpenCV
+
         image = cv2.imread('fichasp.jpeg')
-
-        # Convierte la imagen a escala de grises
-        #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Aplica la detección de bordes con Canny
         edges = cv2.Canny(image, 40, 150)
-
-        # Aplica la transformada de Hough circular
         circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=30, minRadius=10, maxRadius=30)
 
-        # Contadores para círculos negros y blancos
         chipsCount = 0
         blackCount = 0
         whiteCount = 0
-        # Filtra y cuenta los círculos por color
+
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
-                # Obtén el color del centro del círculo
                 color = image[i[1], i[0]]
-
-                # Filtra y cuenta por color (negro o blanco)
                 if count_type == "WHITE":
                     if np.mean(color) > 128:
                         chipsCount += 1
@@ -64,17 +53,13 @@ def modificar_imagen():
                         whiteCount += 1
                         cv2.circle(image, (i[0], i[1]), i[2], (255, 0, 0), 2)
                         cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
-
-        # Guarda la imagen en BytesIO
+                        
         imagenRgb = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
         img_bytes_io = BytesIO()
         plt.imsave(img_bytes_io, imagenRgb)
         img_bytes_io.seek(0)
-
-        # Imprime los resultados en la terminal
         print(f"En el tablero se puede ver la cantidad de fichas negras: {chipsCount}")
-
-        # Devuelve la imagen como archivo
+        
         response = make_response(send_file(img_bytes_io, mimetype='image/jpeg'))
         response.headers['blackCount'] = blackCount
         response.headers['whiteCount'] = whiteCount
